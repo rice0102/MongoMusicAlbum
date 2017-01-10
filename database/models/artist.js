c2b47ac95b1ac56b0d8b4cc5636b4b139027ce05
bigrice0102 <yilinyeh0102@gmail.com>
2017-01-10 15:26:48 +0800

    delete, create,find

--- database/models/artist.js
+++ database/models/artist.js
@@ -1,1 +1,29 @@
 // Todo: Create Artist Model
+const mongoose = require('mongoose');
+const AlbumSchema = require('./album');
+
+const Schema = mongoose.Schema;
+
+const ArtistSchema = new Schema({
+  name: {
+    type: String,
+    validate: {
+        validator: (name) => name.length > 2,
+        message: 'Name must be longer than 2 characters.'
+    },
+    required: [true, 'Name is required.']
+  },
+  age: Number,
+  yearsActive: Number,
+  image: String,
+  genre: String,
+  website: String,
+  netWorth: Number,
+  labelName: String,
+  retired: Boolean,
+  albums: [AlbumSchema]
+});
+
+const Artist = mongoose.model('artist', ArtistSchema);
+
+module.exports = Artist;
